81dd8ebfdb10bc65b4bc82f93deb8a2e53614f82
Devin Droddy <devin.droddy@gmail.com>
2024-12-10 07:53:59 -0500

    better package.nix
    updated to meet standards of nixos reviewer

--- package.nix
+++ package.nix
@@ -1,23 +1,45 @@
-{pkgs, ...}:
-pkgs.stdenv.mkDerivation {
-  name = "syspower";
-  src = pkgs.fetchFromGitHub {
-    owner = "System64Fumo";
+{
+  stdenv,
+  lib,
+  fetchFromGitHub,
+  makeWrapper,
+  pkg-config,
+  gtkmm4,
+  gtk4-layer-shell,
+}:
+stdenv.mkDerivation {
+  pname = "syspower";
+  version = "0-unstable-2024-12-10";
+
+  src = fetchFromGitHub {
+    owner = "System64fumo";
     repo = "syspower";
     rev = "b205c3443daaee6c3454f23b93b78f42c10221a1";
     hash = "sha256-kLaWIaMTRurY7a8Nn7C9ft/Zdvx35AiU7Db8n/NFy90=";
   };
 
-  buildInputs = with pkgs; [gtkmm4 gtk4-layer-shell];
-  nativeBuildInputs = with pkgs; [
-    pkg-config
-    makeWrapper
+  buildInputs = [
+    gtkmm4
+    gtk4-layer-shell
   ];
 
-  installPhase = ''
-    install -D -t $out/bin syspower
-    install -D -t $out/lib libsyspower.so
-    install -D -t $out/share/sys64/power config.conf style.css
+  nativeBuildInputs = [
+    makeWrapper
+    pkg-config
+  ];
+
+  makeFlags = ["PREFIX=${placeholder "out"}"];
+
+  postFixup = ''
     wrapProgram $out/bin/syspower --prefix LD_LIBRARY_PATH : $out/lib
   '';
+
+  meta = {
+    description = "Simple power menu/shutdown screen for Hyprland";
+    homepage = "https://gihub.com/System64fumo/syspower";
+    license = lib.licenses.wtfpl;
+    maintainers = with lib.maintainers; [justdeeevin];
+    mainProgram = "syspower";
+    platforms = lib.platforms.linux;
+  };
 }
